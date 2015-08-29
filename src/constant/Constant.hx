0ee39cd9bedaa3a1aa5ebfd997045567cc12c3c0
tontien <alexandre.nguyencaotruong@gmail.com>
2015-08-29 11:52:53 +0200

    Update Constant.hx

--- src/constant/Constant.hx
+++ src/constant/Constant.hx
@@ -6,8 +6,10 @@ package constant;
  */
 class Constant
 {
-	
+	// Put your API key here
 	public static inline var API_KEY:String = "MORDE-IS-THE-KING";
+	
+	// Url servers for the regions
 	public static inline var EUW_SERVER:String = "euw.api.pvp.net";
 	public static inline var GLOBAL_SERVER:String = "global.api.pvp.net";
 	
