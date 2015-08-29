294147492b806a6557a0446d5acf371694f97dde
tontien <alexandre.nguyencaotruong@gmail.com>
2015-08-29 11:55:03 +0200

    Update ItemDao.hx

--- src/dao/ItemDao.hx
+++ src/dao/ItemDao.hx
@@ -16,11 +16,13 @@ class ItemDao
 		bdd = bd;
 	}
 	
+	// Parsing functions
 	public function insertItem(id:Int, description:String, name:String, group:String, version:String)
 	{
 		bdd.getCnx().request("insert into test.Item (item_id, description, name, group_item, version) values (" + id + ",'" + description +"','" + name +"','" + group +"','" + version +"')");
 	}
 	
+	// UI functions
 	public function selectItems()
 	{
 		return bdd.getCnx().request("SELECT item.item_id, dtw_item14.name, dtw_item11.amount as item_11, dtw_item14.amount as item_14, (dtw_item14.amount-dtw_item11.amount)/dtw_item14.amount as diff FROM dtw_item11, dtw_item14, item where dtw_item11.name = dtw_item14.name and item.name = dtw_item14.name and item.version='5.14.1' HAVING dtw_item11.amount > 0.1 order by diff desc");
